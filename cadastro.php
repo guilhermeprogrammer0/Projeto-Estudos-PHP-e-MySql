9dfcafcc51df80f810c9da1c997d7c5ef6722db4
Your Name <you@example.com>
2022-08-27 15:31:11 -0300

    Ajustando o HTML

--- cadastro.php
+++ cadastro.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Início</title>
+    <title>Página de Cadastro</title>
 </head>
 
 <body class="corpo">
